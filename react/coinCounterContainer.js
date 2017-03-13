8d0ee7ba59de2ac494e77e65fe2c0cc98fe6c391
Damon <damonnguyen92@gmail.com>
2017-03-13 11:21:37 -0700

    Created react directory for all related react files and ccreated simple react app that render Hello World

---
+++ react/coinCounterContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CoinCounterContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="helloworld">Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default CoinCounterContainer;
---
+++ react/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CoinCounterContainer from './CoinCounterContainer.js';
+
+ReactDOM.render(<CoinCounterContainer />, document.getElementById('App'));
