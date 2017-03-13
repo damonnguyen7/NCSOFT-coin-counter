385f6c35858712cc593820ef050b2d969e57d520
Damon <damonnguyen92@gmail.com>
2017-03-13 12:33:30 -0700

    Connect main component to redux store and test if connection is working by using console.log to display current state

--- react/coinCounterContainer.js
+++ react/coinCounterContainer.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class CoinCounterContainer extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    this.setState({
+      coins: this.props.coins
+    });
+  }
+
   render() {
+    console.log('state from redux store: ', this.state.coins);
     return (
-      <div>
-        <h1 className="helloworld">Hello World!</h1>
+      <div className="container main-container">
+        <div className="row">
+          <h1 className="col-xs-12 text-center">Coin Counter</h1>
+        </div>
       </div>
     );
   }
 }
 
-export default CoinCounterContainer;
+const mapStateToProps = function(state) {
+  return {
+    coins: state
+  }
+};
+
+export default connect(mapStateToProps)(CoinCounterContainer);
