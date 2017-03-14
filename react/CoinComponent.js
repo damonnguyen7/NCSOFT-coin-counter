2f7ff3a58a81a89763433c98e96444d9a8f2d3d7
Damon <damonnguyen92@gmail.com>
2017-03-14 11:52:35 -0700

    Define clearCounter to clear the counter when editing coins, update setInputValue to display warning when input is not a number and update HTML by changing span to div

--- react/CoinComponent.js
+++ react/CoinComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { defineCoin } from './redux/actions/index.js';
+import { defineCoin, defineCounter } from './redux/actions/index.js';
 import { connect } from 'react-redux';
 
-class CoinContainer extends Component {
+class CoinComponent extends Component {
   constructor() {
     super();
     this.state = {};
@@ -12,6 +12,7 @@ class CoinContainer extends Component {
     this.setInputValueD = this.setInputValueD.bind(this);
     this.setInputValueC = this.setInputValueC.bind(this);
     this.stopProp = this.stopProp.bind(this);
+    this.clearCounter = this.clearCounter.bind(this);
   }
 
   componentWillMount() {
@@ -26,6 +27,7 @@ class CoinContainer extends Component {
   displayCoinInput() {
     var target = event.srcElement;
     this.props.dispatch(defineCoin(target.id, this.state['inputValue' + target.id], true));
+    this.clearCounter();
   }
 
   defineCoinOnSubmit(position, proxy, event) {
@@ -37,28 +39,56 @@ class CoinContainer extends Component {
     e.stopPropagation();
   }
 
+  clearCounter() {
+    var positions = ['A', 'B', 'C', 'D'];
+    for (var i = 0; i < positions.length; i++) {
+      var position = positions[i];
+      this.props.dispatch(defineCounter(position, 0, false));
+    }
+  }
+
   setInputValueA(e) {
-    this.setState({
-      inputValueA: e.target.value
-    });
+    var isNumber = Number(e.target.value);
+    if (!isNaN(isNumber)) {
+      this.setState({
+        inputValueA: e.target.value
+      });
+    } else {
+      alert('Input value must be a number!');
+    }
   }
 
   setInputValueB(e) {
-    this.setState({
-      inputValueB: e.target.value
-    });
+    var isNumber = Number(e.target.value);
+    if (!isNaN(isNumber)) {
+      this.setState({
+        inputValueB: e.target.value
+      });
+    } else {
+      alert('Input value must be a number!');
+    }
   }
 
   setInputValueC(e) {
-    this.setState({
-      inputValueC: e.target.value
-    });
+    var isNumber = Number(e.target.value);
+    if (!isNaN(isNumber)) {
+      this.setState({
+        inputValueC: e.target.value
+      });
+    } else {
+      alert('Input value must be a number!');
+    }
   }
 
   setInputValueD(e) {
-    this.setState({
-      inputValueD: e.target.value
-    });
+    var isNumber = Number(e.target.value);
+    if (!isNaN(isNumber)) {
+      this.setState({
+        inputValueD: e.target.value
+      });
+    } else {
+      alert('Input value must be a number!');
+    }
   }
 
   render() {
@@ -67,76 +97,76 @@ class CoinContainer extends Component {
         <div className="col-xs-6 xs-square">
             <div className="position-a text-center" onClick={this.displayCoinInput}>
               {
-                //display counter:
+                //If show is true and counter value is greater than 0 ? show counter else return null
                 this.props.coins.coinA.counter.show && this.props.coins.coinA.counter.value > 0 ?
-                <div className="span-counter-value-container">
-                  <div className="span-counter-value">{this.props.coins.coinA.counter.value}</div>
+                <div className="div-counter-value-container">
+                  <div className="div-counter-value" id="A">{this.props.coins.coinA.counter.value}</div>
                 </div> : null
               }
               {
-                //display coin value or form:
+                //display coin value or form
                 this.props.coins.coinA.valueOfCoin.show ?
                 <form className='position-a-input col-xs-6 col-xs-offset-3' onSubmit={this.defineCoinOnSubmit.bind(this, 'A')}>
                   <input type="text" value={this.state.inputValueA} onClick={this.stopProp} onChange={this.setInputValueA} className="form-control input-sm col-xs-3 text-center" />
-                  <input type="submit" value="ok" className="btn btn-default col-xs-12" onClick={this.stopProp} />
-                </form> : <div className="span-coin-value" id="A" onClick={this.displayCoinInput}>{this.props.coins.coinA.valueOfCoin.value}</div>
+                  <input type="submit" value="ok" className="btn btn-success col-xs-12" onClick={this.stopProp} />
+                </form> : <div className="div-coin-value" id="A" onClick={this.displayCoinInput}>{this.props.coins.coinA.valueOfCoin.value}</div>
               }
             </div>
           </div>
           <div className="col-xs-6 xs-square">
             <div className="position-b text-center" onClick={this.displayCoinInput}>
               {
-                //display counter:
+                //If show is true and counter value is greater than 0 ? show counter else return null
                 this.props.coins.coinB.counter.show && this.props.coins.coinB.counter.value > 0 ?
-                <div className="span-counter-value-container">
-                  <div className="span-counter-value" id="B">{this.props.coins.coinB.counter.value}</div>
+                <div className="div-counter-value-container">
+                  <div className="div-counter-value" id="B">{this.props.coins.coinB.counter.value}</div>
                 </div> : null
               }
               {
-                //display coin value or form:
+                //display coin value or form
                 this.props.coins.coinB.valueOfCoin.show ?
                 <form className='position-b-input col-xs-6 col-xs-offset-3' onSubmit={this.defineCoinOnSubmit.bind(this, 'B')}>
                   <input type="text" value={this.state.inputValueB} onClick={this.stopProp} onChange={this.setInputValueB} className="form-control input-sm col-xs-3 text-center" />
-                  <input type="submit" value="ok" className="btn btn-default col-xs-12" onClick={this.stopProp} />
-                </form> : <div className="span-coin-value" id="B" onClick={this.displayCoinInput}>{this.props.coins.coinB.valueOfCoin.value}</div>
+                  <input type="submit" value="ok" className="btn btn-danger col-xs-12" onClick={this.stopProp} />
+                </form> : <div className="div-coin-value" id="B" onClick={this.displayCoinInput}>{this.props.coins.coinB.valueOfCoin.value}</div>
               }
             </div>
           </div>
           <div className="col-xs-6 xs-square">
             <div className="position-c text-center" onClick={this.displayCoinInput}>
               {
-                //display counter:
+                //If show is true and counter value is greater than 0 ? show counter else return null
                 this.props.coins.coinC.counter.show && this.props.coins.coinC.counter.value > 0 ?
-                <div className="span-counter-value-container">
-                  <div className="span-counter-value" id="C">{this.props.coins.coinC.counter.value}</div>
+                <div className="div-counter-value-container">
+                  <div className="div-counter-value" id="C">{this.props.coins.coinC.counter.value}</div>
                 </div> : null
               }
               {
-                //display coin value or form:
+                //display coin value or form
                 this.props.coins.coinC.valueOfCoin.show ?
                 <form className='position-c-input col-xs-6 col-xs-offset-3' onSubmit={this.defineCoinOnSubmit.bind(this, 'C')}>
                   <input type="text" value={this.state.inputValueC} onClick={this.stopProp} onChange={this.setInputValueC} className="form-control input-sm col-xs-3 text-center" />
-                  <input type="submit" value="ok" className="btn btn-default col-xs-12" onClick={this.stopProp} />
-                </form> : <div className="span-coin-value" id="C" onClick={this.displayCoinInput}>{this.props.coins.coinC.valueOfCoin.value}</div>
+                  <input type="submit" value="ok" className="btn btn-primary col-xs-12" onClick={this.stopProp} />
+                </form> : <div className="div-coin-value" id="C" onClick={this.displayCoinInput}>{this.props.coins.coinC.valueOfCoin.value}</div>
               }
             </div>
           </div>
           <div className="col-xs-6 xs-square">
             <div className="position-d text-center" onClick={this.displayCoinInput}>
               {
-                //display counter:
+                //If show is true and counter value is greater than 0 ? show counter else return null
                 this.props.coins.coinD.counter.show && this.props.coins.coinD.counter.value > 0 ?
-                <div className="span-counter-value-container">
-                  <div className="span-counter-value" id="D">{this.props.coins.coinD.counter.value}</div>
+                <div className="div-counter-value-container">
+                  <div className="div-counter-value" id="D">{this.props.coins.coinD.counter.value}</div>
                 </div> : null
               }
               {
-                //display coin value or form:
+                //display coin value or form
                 this.props.coins.coinD.valueOfCoin.show ?
                 <form className='position-d-input col-xs-6 col-xs-offset-3' onSubmit={this.defineCoinOnSubmit.bind(this, 'D')}>
                   <input type="text" value={this.state.inputValueD} onClick={this.stopProp} onChange={this.setInputValueD} className="form-control input-sm col-xs-3 text-center" />
-                  <input type="submit" value="ok" className="btn btn-default col-xs-12" onClick={this.stopProp} />
-                </form> : <div className="span-coin-value" id="D" onClick={this.displayCoinInput}>{this.props.coins.coinD.valueOfCoin.value}</div>
+                  <input type="submit" value="ok" className="btn btn-warning col-xs-12" onClick={this.stopProp} />
+                </form> : <div className="div-coin-value" id="D" onClick={this.displayCoinInput}>{this.props.coins.coinD.valueOfCoin.value}</div>
               }
             </div>
           </div>
@@ -149,4 +179,4 @@ let mapStateToProps = (state) => ({
   coins: state
 });
 
-export default connect(mapStateToProps)(CoinContainer);
+export default connect(mapStateToProps)(CoinComponent);
